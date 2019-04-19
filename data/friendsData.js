a49d60e575f392414cc6cd20ad6bb799c3994e7d
Terry <tsouwannamuang@gmail.com>
2019-04-19 14:23:09 -0700

    commit

--- data/friendsData.js
+++ data/friendsData.js
@@ -13,8 +13,55 @@ module.exports = friendsData = [{
         5,
         4,
         1
-      ]
+      ],
   
+      name: "Bugssy",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        3,
+        4,
+        4,
+        4,
+        2,
+        5,
+        2,
+        5,
+        3,
+        1
+      ],
+
+      name: "Porky",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        5,
+        3,
+        4,
+        4,
+        2,
+        1,
+        2,
+        5,
+        3,
+        5
+      ],
+
+      name: "Daffy Duck",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        1,
+        3,
+        4,
+        3,
+        2,
+        3,
+        3,
+        4,
+        5,
+        5
+      ],
+
+
+
 },
 
 ];
