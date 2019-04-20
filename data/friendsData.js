baaed688bbf273e6df9222c9dec7cd32248717ea
Terry <tsouwannamuang@gmail.com>
2019-04-19 21:31:23 -0700

    commit

--- data/friendsData.js
+++ data/friendsData.js
@@ -1,67 +1,29 @@
-module.exports = friendsData = [{
-
+module.exports = friendsData = [
+  {
     name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-  
-      name: "Bugssy",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        3,
-        4,
-        4,
-        4,
-        2,
-        5,
-        2,
-        5,
-        3,
-        1
-      ],
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
 
-      name: "Porky",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        3,
-        4,
-        4,
-        2,
-        1,
-        2,
-        5,
-        3,
-        5
-      ],
+  {
+    name: "Bugssy",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 4, 4, 4, 2, 5, 2, 5, 3, 1]
+  },
 
-      name: "Daffy Duck",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        1,
-        3,
-        4,
-        3,
-        2,
-        3,
-        3,
-        4,
-        5,
-        5
-      ],
+  {
+    name: "Porky",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 3, 4, 4, 2, 1, 2, 5, 3, 5]
+  },
 
-
-
-},
-
+  {
+    name: "Daffy Duck",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 3, 4, 3, 2, 3, 3, 4, 5, 5]
+  }
 ];
